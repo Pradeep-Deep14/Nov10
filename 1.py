fc47039c062f6bc056e8e36e550c33b8ed2b7839
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-10 08:54:00 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+def modify_list_fruits(fruits):
+    fruit_set=set(fruits)
+    fruit_set.add("pear")
+    fruit_set.remove("apple")
+    fruit_list=list(fruit_set)
+    return fruit_list[1]
+fruits=["apple","banana","orange"]
+print(modify_list_fruits(fruits))
+
+
+#impossible to return for sure
+#will return different outputs
---
+++ 2.py
@@ -0,0 +1,6 @@
+numbers=[1,2,3,4,5]
+squared_numbers=[num**2 for num in numbers]
+print(squared_numbers)
+
+
+#[1, 4, 9, 16, 25]
