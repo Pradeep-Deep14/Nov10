5bad2ec254ddd0a40d3d6a9080d52fed816f165d
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-10 09:19:13 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,10 @@
+def unusual_func(x:list):
+    if all(x):
+        return "Hello"
+    elif not x:
+        return "Hi"
+    else:
+        return "GoodBye"
+    
+lst=[1,2,-3,4,0]
+print(unusual_func(lst))
